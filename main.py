8ae888f48823d73f92d6928cd0c7cddbb7735800
deandreti <136617040+deandreti@users.noreply.github.com>
2025-08-27 09:57:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import random
 projects = ["Project A", "Project B", "Project C", "Project D"]
 print("3 dự án ngẫu nhiên:", random.sample(projects, 3))
 
---02--05--06--08--11--12--16--21--22--24--26
+--02--05--06--08--11--12--16--21--22--24--26--27
